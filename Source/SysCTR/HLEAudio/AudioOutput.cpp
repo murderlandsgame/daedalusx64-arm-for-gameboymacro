8a2c31cd738e1892036ed0e9fe33be50f168a0f6
wtfloltk <153698146+wtfloltk@users.noreply.github.com>
2024-01-13 04:24:44 -0500

    Update AudioOutput.cpp

--- Source/SysCTR/HLEAudio/AudioOutput.cpp
+++ Source/SysCTR/HLEAudio/AudioOutput.cpp
@@ -35,7 +35,7 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 
 extern u32 gSoundSync;
 
-static const u32 DESIRED_OUTPUT_FREQUENCY = 45666;
+static const u32 DESIRED_OUTPUT_FREQUENCY = 44100;
 
 static const u32 CTR_BUFFER_SIZE  = 1024 * 2;
 static const u32 CTR_BUFFER_COUNT = 6;
